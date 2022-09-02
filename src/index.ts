1104b191468a60cff3414e2b856201470e8e8057
Sibi <sibivel979@gmail.com>
2022-09-02 17:39:26 -0400

    clean up

--- src/index.ts
+++ src/index.ts
@@ -24,21 +24,3 @@ window.addEventListener('load', () => {
 
   var game = new Game(config);
 });
-
-// Create a simple model.
-// const model = tf.sequential();
-// console.log(model);
-// model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-// console.log(model.getWeights().toString());
-// console.log(model);
-
-// // Prepare the model for training: Specify the loss and the optimizer.
-// model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-// // Generate some synthetic data for training. (y = 2x - 1)
-// const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
-// const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
-
-// // Train the model using the data.
-// model.fit(xs, ys, {epochs: 250});
-// console.log(model.predict(tf.tensor2d([20], [1, 1])).toString());
